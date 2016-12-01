f9c11942c58e9d607e1ea208fed01bfe5dfb6d11
Arek Mytych <amytych@gmail.com>
2016-12-01 10:15:22 +0100

    Documents year toggling

--- client/containers/Society.js
+++ client/containers/Society.js
@@ -1,5 +1,10 @@
 import React from "react"
 import { connect } from "react-redux"
+import Select from "react-select"
+import maxBy from "lodash/fp/maxBy"
+import map from "lodash/fp/map"
+import filter from "lodash/fp/filter"
+import uniqBy from "lodash/fp/uniqBy"
 
 import {
   makeGetSociety,
@@ -13,15 +18,44 @@ import {
 } from "../actions/appActions"
 
 class Society extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+
+    this.state = {
+      year: +maxBy(d => +d.year, props.documents).year,
+    }
+
+    this.handleYearChange = this.handleYearChange.bind(this)
+  }
+
+  handleYearChange(year) {
+    this.setState({ year: year.value })
+  }
+
   render() {
+    const { year } = this.state
     const { society, data, documents } = this.props
+    const yearOption = d => ({ value: +d.year, label: d.year })
+    const yearOptions = uniqBy("value", map(yearOption, documents))
+    const yearFilter = d => +d.year === +year
+    const yearDocuments = filter(yearFilter, documents)
 
     return (
       <div className="py4 pl2">
         <h1>{ "National Society" }</h1>
         <div>{ `${society.NSO_DON_name} (${society.KPI_DON_Code}) joined ${society.admission_date}` }</div>
-        <h2>{ "Documents" } <small>{ `(${documents.length} records)` }</small></h2>
-        <pre>{ JSON.stringify(documents, null, 2) }</pre>
+        <h2 style={{ float: "left" }}>{ "Documents" }</h2>
+        <div style={{ float: "left", minWidth: "110px" }}>
+          <Select
+            searchable={ false }
+            clearable={ false }
+            name="year-selector"
+            value={ year }
+            options={ yearOptions }
+            onChange={ this.handleYearChange }
+          />
+        </div>
+        <pre style={{ clear: "left" }}>{ JSON.stringify(yearDocuments, null, 2) }</pre>
         <h2>{ "Data" } <small>{ `(${data.length} records)` }</small></h2>
         <pre>{ JSON.stringify(data, null, 2) }</pre>
       </div>
