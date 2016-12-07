5135f60798f528876d3c1e2edfad3595f97d1bea
zimrick <r.zimerman33@gmail.com>
2016-12-07 16:43:11 +0100

    Added more sections to societies page

--- client/containers/Societies.js
+++ client/containers/Societies.js
@@ -8,14 +8,6 @@ import { fetchNationalSocieties } from "../actions/appActions"
 class Societies extends React.Component {
   render() {
     const { language } = this.context.i18n
-    const societies = this.props.nationalSocieties.map(s =>
-      <li key={ s.KPI_DON_Code }>
-        <Link to={ prefixLanguageToRoute(language, `/societies/${s.slug}`) }>
-          { s.NSO_DON_name }
-        </Link>
-      </li>
-    )
-
     const societiesPerCol = Math.ceil(this.props.nationalSocieties.length / 3)
 
     return (
@@ -52,8 +44,8 @@ class Societies extends React.Component {
                 <div className="col sm-4 px1">
                   {
                     this.props.nationalSocieties.slice(0,societiesPerCol).map((society, i) => (
-                      <li className="block py05" key={i}>
-                        <Link to={`/societies/${society.slug}`}>{ society.NSO_DON_name }</Link>
+                      <li className="block py05" key={society.KPI_DON_Code}>
+                        <Link to={prefixLanguageToRoute(language, `/societies/${society.slug}`)}>{ society.NSO_DON_name }</Link>
                       </li>
                     ))
                   }
@@ -61,8 +53,8 @@ class Societies extends React.Component {
                 <div className="col sm-4 px1">
                   {
                     this.props.nationalSocieties.slice(societiesPerCol,societiesPerCol*2).map((society, i) => (
-                      <li className="block py05" key={i}>
-                        <Link to={`/societies/${society.slug}`}>{ society.NSO_DON_name }</Link>
+                      <li className="block py05" key={society.KPI_DON_Code}>
+                        <Link to={prefixLanguageToRoute(language, `/societies/${society.slug}`)}>{ society.NSO_DON_name }</Link>
                       </li>
                     ))
                   }
@@ -70,8 +62,8 @@ class Societies extends React.Component {
                 <div className="col sm-4 px1">
                   {
                     this.props.nationalSocieties.slice(societiesPerCol*2,societiesPerCol*3).map((society, i) => (
-                      <li className="block py05" key={i}>
-                        <Link to={`/societies/${society.slug}`}>{ society.NSO_DON_name }</Link>
+                      <li className="block py05" key={society.KPI_DON_Code}>
+                        <Link to={prefixLanguageToRoute(language, `/societies/${society.slug}`)}>{ society.NSO_DON_name }</Link>
                       </li>
                     ))
                   }
@@ -81,13 +73,29 @@ class Societies extends React.Component {
           </div>
         </div>
 
-        { /* <ul>
-          {
-            this.props.nationalSocieties.map((society, i) => {
+        <div className='px1 py4 bg-beige'>
+          <div className='clearfix mxn1'>
+            <div className='col sm-4 sm-offset-6 px1'>
+              <h2 className='headline sm-display-1 light mt0'>{ "The IFRC at a glance" }</h2>
+              <p className='lead'>{ "Get the big picture with the IFRC at a glance, and see how the largest humanitarian network looks." }</p>
+              <Link to='/' className='btn btn--raised bg-primary'>
+                <span className='block py05 px1'>{ "Explore the IFRC" }</span>
+              </Link>
+            </div>
+          </div>
+        </div>
 
-            })
-          }
-        </ul> */ }
+        <div className='px1 py4 bg-secondary'>
+          <div className='clearfix mxn1'>
+            <div className='col sm-10 sm-offset-1 px1'>
+              <h2 className='headline sm-display-1 light mt0'>{ "For data collectors" }</h2>
+              <p className='lead'>{ "To get started with the data collection for your National Society, please log in." }</p>
+              <Link to='/' className='btn btn--raised bg-primary'>
+                <span className='block py05 px1'>{ "Login as data collector" }</span>
+              </Link>
+            </div>
+          </div>
+        </div>
       </section>
     )
   }
