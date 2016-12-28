9a6c83a2d5dba87933c778e9edd9b35fce939c7f
zimrick <r.zimerman33@gmail.com>
2016-12-28 16:45:05 +0100

    Minor styling improvements

--- client/components/Globe.js
+++ client/components/Globe.js
@@ -48,18 +48,23 @@ class Globe extends React.Component {
   render() {
     return (
       <div className="relative ratio-1-1 overflow-hidden">
-        <div className="absolute t0 l0 r0 b0 overflow-hidden">
         {
           this.state.loading ? (
-            <p className="text-center">{ "Loading map..." }</p>
-          ) : (
+            <div className="absolute t0 l0 r0 b0 base-12 overflow-hidden">
+              <p className="relative text-center t50">{ "Loading map..." }</p>
+            </div>
+          ) : null
+        }
+        <div className="absolute t0 l0 r0 b0 overflow-hidden" style={{opacity: this.state.loading ? 0 : 1, transform: this.state.loading ? "rotate(10deg)" : "rotate(0)", transition: "all 1s"}}>
+        {
+          !this.state.loading ? (
             <svg width={200} height={200} viewBox="0 0 200 200">
               {this.state.countries.map((country, i) => (
                 <path key={ i } d={geoPath().projection(this.projection())(country)} style={{fill: "#EFEBE9"}} />
               ))}
               <circle cx={100} cy={100} r={8} style={{fill: "rgba(208,2,27,0.8)", stroke: "#fff", strokeWidth: "2px"}} />
             </svg>
-          )
+          ) : null
         }
         </div>
       </div>
