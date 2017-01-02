9b60c30de726e235db2c860dedd8fc61bcac2466
zimrick <r.zimerman33@gmail.com>
2017-01-02 17:44:04 +0100

    Implemented collapsibles for FAQ page

--- client/containers/FAQ.js
+++ client/containers/FAQ.js
@@ -1,10 +1,85 @@
 
 import React from "react"
+import Breadcrumbs from "../components/Breadcrumbs"
+
+const CollapsibleHeader = ({ children }) => <span>{ children }</span>
+const CollapsibleBody = ({ children }) => <span>{ children }</span>
+
+class Collapsible extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { expanded: false }
+
+    this.toggleCollapsible = this.toggleCollapsible.bind(this)
+  }
+  toggleCollapsible() {
+    this.setState({ expanded: !this.state.expanded })
+  }
+  render() {
+    return (
+      <article className="shadow-4 bg-white mb1">
+        <header className="p1" style={{ cursor: "pointer" }} onClick={ this.toggleCollapsible }>
+          { this.props.children[0] }
+        </header>
+        <div className="p1 bg-secondary" style={{ display: this.state.expanded ? "block" : "none" }}>
+          { this.props.children[1] }
+        </div>
+      </article>
+    )
+  }
+}
 
 class FAQ extends React.Component {
   render() {
     return (
-      <div>FAQ page</div>
+      <section>
+        <Breadcrumbs links={[
+          { name: "Home", path: "/" },
+          { name: "FAQ", path: "/faq" },
+        ]}/>
+        <div className="px1">
+          <div className="clearfix mxn1">
+            <header className="col sm-6 sm-offset-3 px1 pt1">
+              <h1 className="display-1 md-display-2 m0 light">{ "FAQ" }</h1>
+            </header>
+          </div>
+
+          <div className="clearfix mxn1 pb4">
+            <div className="col sm-6 sm-offset-3 px1 pt1">
+              <p className="lead">{ "This text will be the introduction for the FAQ page. Below you will find the most frequently asked questions. If your question has not been answered below, please contact the IFRC secreatariat at: " }<span className="color-primary">{ "fdrs@ifrc.org" }</span></p>
+
+              <Collapsible>
+                <CollapsibleHeader>
+                  <h1 className="headline m0">{ "1. Question goes here" }</h1>
+                </CollapsibleHeader>
+                <CollapsibleBody>
+                  <p>{ "The answer will go here, but will be hidden by default." }</p>
+                </CollapsibleBody>
+              </Collapsible>
+
+              <Collapsible>
+                <CollapsibleHeader>
+                  <h1 className="headline m0">{ "2. Second question can go here" }</h1>
+                </CollapsibleHeader>
+                <CollapsibleBody>
+                  <p>{ "The answer will go here, but will be hidden by default." }</p>
+                </CollapsibleBody>
+              </Collapsible>
+
+              <Collapsible>
+                <CollapsibleHeader>
+                  <h1 className="headline m0">{ "3. Another question goes here" }</h1>
+                </CollapsibleHeader>
+                <CollapsibleBody>
+                  <p>{ "The answer will go here, but will be hidden by default." }</p>
+                </CollapsibleBody>
+              </Collapsible>
+
+            </div>
+          </div>
+
+        </div>
+      </section>
     )
   }
 }
