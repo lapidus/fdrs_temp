195b9e47511582b8332f0d2671fb3315fc2f8580
Daniel Lapidus <daniel@lapidus.se>
2017-01-05 11:28:22 +0100

    Added files and hooked up translations for Data Download

--- client/containers/DataDownload.js
+++ client/containers/DataDownload.js
@@ -2,8 +2,13 @@
 import React from "react"
 import Breadcrumbs from "../components/Breadcrumbs"
 
+import { translate } from "react-i18next"
+
+
 class Download extends React.Component {
   render() {
+    const { t } = this.props
+    const { language } = this.context.i18n
     return (
       <section>
         <Breadcrumbs links={[
@@ -13,18 +18,23 @@ class Download extends React.Component {
         <div className="px1">
           <div className="clearfix mxn1">
             <header className="col sm-6 sm-offset-3 px1 pt1">
-              <h1 className="display-1 md-display-2 m0 light">{ "Data download" }</h1>
+              <h1 className="display-1 md-display-2 m0 light">{ t("data-download:title") } </h1>
             </header>
           </div>
 
           <div className="clearfix mxn1 pb4">
             <div className="col sm-6 sm-offset-3 px1 pt1">
-              <p className="lead">{ "On this page you can download the datasets used for the various visualizations on FDRS." }</p>
-              <button className="btn bg-primary">{ "Download csv" }</button><br /><br />
-              <button className="btn bg-primary">{ "Download excel" }</button>
-              <p>{ "More text can go here. If necessary we can drop in visuals and other small bits and pieces of content in here." }</p>
-              <p>{ "More text can go here. If necessary we can drop in visuals and other small bits and pieces of content in here. We need a little bit more text here in order to fill up the page." }</p>
-              <p>{ "More text can go here. If necessary we can drop in visuals and other small bits and pieces of content in here." }</p>
+              <p className="lead">{ t("data-download:intro") }</p>
+              <button className="btn bg-primary">
+                <a href="downloads/FDRS_DATASET.csv">
+                  { "DOWNLOAD CSV FILE" }
+                </a>
+              </button><br /><br />
+              <button className="btn bg-primary">
+                <a href="downloads/FDRS_DATASET.csv">
+                  { "DOWNLOAD EXCEL FILE" }
+                </a>
+              </button>
             </div>
           </div>
         </div>
@@ -33,4 +43,13 @@ class Download extends React.Component {
   }
 }
 
-export default Download
+
+Download.propTypes = {
+  t: React.PropTypes.func.isRequired,
+}
+
+Download.contextTypes = {
+  i18n: React.PropTypes.object,
+}
+
+export default translate("data-download")(Download)
