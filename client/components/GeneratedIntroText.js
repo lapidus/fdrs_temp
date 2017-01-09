fa5688500a5818fe6d7241945adcc753068b9b1e
zimrick <r.zimerman33@gmail.com>
2017-01-09 17:54:30 +0100

    Society refactor and translation strings

---
+++ client/components/GeneratedIntroText.js
@@ -0,0 +1,36 @@
+
+import React from "react"
+import niceNum from "../utils/niceNum"
+
+class GeneratedIntroText extends React.Component {
+  render() {
+
+    const {
+      societyName,
+      admissionDate,
+      latestData,
+      earliestData,
+      translationText,
+    } = this.props
+
+    const latestTotal = latestData.KPI_noPeopleVolunteering
+    const earliestTotal = earliestData.KPI_noPeopleVolunteering
+
+    const latestMale = latestData.KPI_noPeopleVolunteeringM
+    const latestFemale = latestData.KPI_noPeopleVolunteeringF
+
+    const latestYear = latestData.KPI_Year
+    const earliestYear = earliestData.KPI_Year
+
+    const admissionString = `${societyName} ${translationText[0]} ${admissionDate}.`
+    const volunteerString = latestTotal ? `${translationText[1]} ${latestYear}${translationText[2]} ${niceNum(latestTotal, 0, null, true)} ${translationText[3]}` : null
+    const comparisonString = earliestTotal ? ` (${translationText[4]} ${niceNum(earliestTotal, 0, null, true)} ${translationText[5]} ${earliestYear})` : null
+    const genderString = latestMale && latestFemale ? `${translationText[6]} ${Math.round(100 / latestTotal * latestMale)}% ${translationText[7]} ${Math.round(100 / latestTotal * latestFemale)}% ${translationText[8]}` : null
+
+    const part2 = volunteerString ? ` ${volunteerString}${comparisonString}${genderString}.` : ""
+
+    return <span>{ admissionString }{ part2 }</span>
+  }
+}
+
+export default GeneratedIntroText
