13d67c00d028185865eec67282987b6d9026931c
Daniel Lapidus <daniel@lapidus.se>
2017-01-19 09:03:33 +0100

    New dataset and fixed download link

--- client/containers/DataDownload.js
+++ client/containers/DataDownload.js
@@ -26,7 +26,7 @@ class Download extends React.Component {
             <div className="col sm-6 sm-offset-3 px1 pt1">
               <p className="lead">{ t("data-download:intro") }</p>
               <button className="btn bg-primary">
-                <a href="downloads/FDRS_DATASET.csv" className="px1">
+                <a href="/api/indicators/time_series.csv" className="px1">
                   <span>
                     <svg style={{width:16,height:16,marginTop:-1,marginRight:16}} width="24px" height="24px" viewBox="0 0 24 24">
                       <g transform="translate(0, 0)" style={{stroke:"currentcolor"}}>
@@ -39,20 +39,7 @@ class Download extends React.Component {
                   { "DOWNLOAD CSV FILE" }
                 </a>
               </button><br /><br />
-              <button className="btn bg-primary">
-                <a href="downloads/FDRS_DATASET.csv" className="px1">
-                  <span>
-                    <svg style={{width:16,height:16,marginTop:-1,marginRight:16}} width="24px" height="24px" viewBox="0 0 24 24">
-                      <g transform="translate(0, 0)" style={{stroke:"currentcolor"}}>
-                        <line fill="none" strokeWidth="2" strokeMiterlimit="10" x1="12" y1="9" x2="12" y2="22" strokeLinejoin="miter" strokeLinecap="butt"/>
-                        <polyline fill="none" strokeWidth="2" strokeLinecap="square" strokeMiterlimit="10" points="15,19 12,22 9,19 " strokeLinejoin="miter"/>
-                        <path fill="none" strokeWidth="2" strokeLinecap="square" strokeMiterlimit="10" d="M17,16h2c2.209,0,4-1.791,4-4c0-2.197-1.782-4.013-4.025-3.997C18.718,4.093,15.474,1,11.5,1C7.481,1,4.21,4.164,4.018,8.136C2.287,8.575,1,10.132,1,12c0,2.209,1.791,4,4,4h2" strokeLinejoin="miter"/>
-                      </g>
-                    </svg>
-                  </span>
-                  { "DOWNLOAD EXCEL FILE" }
-                </a>
-              </button>
+
             </div>
           </div>
         </div>
