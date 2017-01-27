c15f2f4c27fd76f428f2f623975681b03c0a10fd
Daniel Lapidus <daniel@lapidus.se>
2017-01-27 18:07:17 +0100

    Fixed broken data link

--- client/utils/NextChapter.js
+++ client/utils/NextChapter.js
@@ -3,12 +3,12 @@ import LanguageLink  from "../components/LanguageLink"
 
 import Icon from "../components/Icon"
 
-const NextChapter = ({ nextChapter }, { language }) =>
+const NextChapter = ({ nextChapter, slug }, { language }) =>
   <div className="clearfix py2 bg-light">
     <div className="col sm-6 sm-offset-6 px1">
       <p className="text-sm color-primary m0">{ nextChapter.heading }</p>
       <h2 className="text-md sm-text-lg mt0 light">
-        <LanguageLink to={ `/fdrs/report/${nextChapter.slug}` }>
+        <LanguageLink to={ slug ?  slug  : `/fdrs/report/${nextChapter.slug}` }>
           { nextChapter.title }
           {/* <Icon name="goto" width={ 30 } height={ 30 } /> */}
         </LanguageLink>
@@ -18,6 +18,7 @@ const NextChapter = ({ nextChapter }, { language }) =>
 
 NextChapter.propTypes = {
   nextChapter: React.PropTypes.object,
+  slug: React.PropTypes.string,
 }
 
 NextChapter.contextTypes = {
